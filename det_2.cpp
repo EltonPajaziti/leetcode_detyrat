edb13e5538f6748e8ddd357cf90afe57f4fef762
EltonPajaziti <pajazitielton002@gmail.com>
2023-01-12 12:59:44 +0100

    ert

---
+++ det_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Rreshti i pare" << endl;
+	cout << "Rreshti i dyte" << endl;
+	cout << endl;
+	cout << "Rreshti i katert" << endl;
+	cout << "Rreshti i peste" << endl;
+	cout << endl;
+	
+	return 0;
+}
