1867b3a486e1e71154b410e548e44b51374425e3
EltonPajaziti <pajazitielton002@gmail.com>
2023-01-12 12:50:45 +0100

    detyra1

---
+++ detyra_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> twoSum(vector<int>& nums, int target) {
+	vector<int> result;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		for (int j = i + 1; j < nums.size(); j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+				result.push_back(i);
+				result.push_back(j);
+				return result;
+			}
+		}
+	}
+	return result;
+
+}
+int main(){
+    vector<int> nums;
+    vector<int> result;
+    int target = 2;
+
+    result = twoSum(nums, target);
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i];
+    }
+    
+
+    return 0;
+}
